17f7a86e055e16c83bd0191325799ff58fa96d8f
sangyeop yoo <vnvj0033@gmail.com>
2021-03-29 12:12:04 +0900

    [07-03번] 문자열 나누고 합치고

--- test/LoopTest.java
+++ test/LoopTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoopTest {
@@ -52,6 +56,19 @@ public class LoopTest {
     @Test
     void testContinue() {
         System.out.println(fiveCountAddStar(12));
+    }
+
+    @Test
+    void testVector() {
+        StringBuilder builder = new StringBuilder();
+        List<String> vector = new Vector<>(Arrays.asList(fiveCountAddStar(12).split(" ")));
+
+        for (int i = 0; i < vector.size(); i++){
+            if (i != 0) builder.append(" ");
+            builder.append(vector.get(i));
+        }
+
+        System.out.println(builder.toString());
     }
 
     String fiveCountAddStar(int n) {
