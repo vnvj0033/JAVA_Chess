4edfea541a17e6106f8be02bdea73085d2ef347c
vnvj0033 <vnvj0033@gmail.com>
2021-04-25 22:23:51 +0900

    [08-2 번] test pass

--- test/BlowsUpTest.java
+++ test/BlowsUpTest.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class BlowsUpTest {
 
     @Test
     void testException() {
-        blowsUp();
+        try {
+            blowsUp();
+            fail();
+        }catch (Exception e){
+
+        }
     }
 
     void blowsUp() {throw new RuntimeException("Somebody showld catch this!");}
